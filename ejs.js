732a9cf5fa596eaa21339d6ae55417be93511f64
filmfuthoem <degaze.film@gmail.com>
2018-08-23 15:55:29 -0700

    post get request and cookie

--- ejs.js
+++ ejs.js
@@ -1,13 +1,50 @@
 let express = require('express');
+let body = require('body-parser');
+let cookie = require('cookie-parser');
 let app = express();
 app.set('view engine','ejs');
 
+// app.get('/',(req,res)=>{
+//   res.render('about');
+// });
+app.use(body());
 app.get('/',(req,res)=>{
-  res.render('about');
+  let data = {name: "Nadtakan",age: 27, job: "Programmer"};
+  res.render('about',{user : data});
 });
 
-app.get('/:name',(req,res)=>{
+app.get('/contact',(req,res)=>{
   let data = {user: "Nadtakan",age: 27, job: "Programmer"};
-  res.render('about',{person : req.params.name, user : data});
+  res.render('contact');
 });
+
+app.get('/db',(req,res)=>{
+  let data = {user: "Nadtakan",age: 27, job: "Programmer"};
+  res.render('db');
+});
+
+app.get('/showForm',(req,res) => {
+  res.render("showForm");
+});
+
+app.post('/formsubmit',(req,res) => {
+   data = {
+     fname : req.body.fname,
+     lname : req.body.lname
+   };
+   console.log(data);
+   // res.end(JSON.stringify(data));
+   res.end("<h1>"+data.fname+"</h1>");
+});
+
+app.get('/createCookie',(req,res)=>{
+  res.cookie('mycookie','Nadtakan');
+  res.end("createCookie");
+});
+
+app.get('/deleteCookie',(req,res)=>{
+  res.clearCookie('mycookie');
+  res.end("Delete cookie completed!");
+});
+
 app.listen(8081);
